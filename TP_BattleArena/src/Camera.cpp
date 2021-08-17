6b428acf041a396ed29c5479ee2845dab9859017
unknown <you@example.com>
2021-08-17 16:45:47 -0400

    camera works git add .! pog

--- TP_BattleArena/src/Camera.cpp
+++ TP_BattleArena/src/Camera.cpp
@@ -6,7 +6,10 @@
 Camera::Camera(Player *p) : p(p) {
 }
 void Camera::move() {
-    gluLookAt(this->p->getX() + sin(p->getAngleRotation() * M_PI / 180) * p->getRadius() / 2, this->p->getY() + this->p->getTaille() + this->p->getRadius() / 2 + cos(p->getAngleRotation() * M_PI / 180) * p->getRadius() / 2,
-              this->p->getZ() + this->p->getRadius(), this->p->getX() + sin(p->getAngleRotation() * M_PI / 180) * 1000,
-              this->p->getY(), this->p->getZ() + cos(p->getAngleRotation() * M_PI / 180) * 1000, 0, 1, 0);
+    float deltaPosX = sin(p->getAngleRotation() * M_PI / 180) * p->getRadius();
+    float posY = this->p->getTaille() + this->p->getRadius();
+    float deltaPosZ =  cos(p->getAngleRotation() * M_PI / 180) * p->getRadius();
+    gluLookAt(this->p->getX() + deltaPosX, this->p->getY() + posY,
+              this->p->getZ() + deltaPosZ, this->p->getX() + deltaPosX * 1000,
+              this->p->getY(), this->p->getZ() + deltaPosZ * 1000, 0, 1, 0);
 }
