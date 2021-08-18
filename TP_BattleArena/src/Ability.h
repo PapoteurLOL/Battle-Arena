cf6963e1c0e32f853f0f2cf25101c5b24fb071f2
unknown <you@example.com>
2021-08-18 10:13:24 -0400

    player ability 0 done

---
+++ TP_BattleArena/src/Ability.h
@@ -0,0 +1,16 @@
+//
+// Created by Administrator on 8/18/2021.
+//
+
+#ifndef TP_BATTLEARENA_ABILITY_H
+#define TP_BATTLEARENA_ABILITY_H
+
+
+
+class Ability {
+
+};
+
+
+
+#endif //TP_BATTLEARENA_ABILITY_H
